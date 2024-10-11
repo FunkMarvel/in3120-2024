1f42fe4682104cb823b956c7e82f2b8e5dfbb7a9
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2024-10-11 17:47:07 +0200

    Update simplesearchengine.py

--- in3120/simplesearchengine.py
+++ in3120/simplesearchengine.py
@@ -3,8 +3,6 @@
 # pylint: disable=too-few-public-methods
 # pylint: disable=too-many-locals
 import collections
-from cgitb import small
-from getpass import fallback_getpass
 from heapq import heappush, heappop
 from typing import Iterator, Dict, Any
 from .sieve import Sieve
@@ -48,48 +46,99 @@ class SimpleSearchEngine:
         N is inferred from the query via the "match_threshold" (float) option, and the maximum number of documents
         to return to the client is controlled via the "hit_count" (int) option.
         """
-        term_counter = collections.Counter(self.__inverted_index.get_terms(query))
-        m = len(term_counter)
-        t = options.setdefault("match_threshold", 0)
-        n = min(1, max(m, int(t * m)))
-        ranking_sieve = Sieve(options.setdefault("hit_count", 1))
 
-        posting_lists = {}
-        smallest_id = -1
-        for term in term_counter.keys():
-            term_iter = self.__inverted_index.get_postings_iterator(term)
-            first_posting = next(term_iter, None)
+        # find terms and multiplicity:
+        term_multiplicity = collections.Counter(self.__inverted_index.get_terms(query))
+        terms = list(term_multiplicity.keys())
+        terms.sort()  # ensure terms are accessed in lexicographical order for each document
+
+        # get parameters for N-out-of-M retrieval:
+        m = len(term_multiplicity)
+        t = options.setdefault("match_threshold", 0.25)  # default threshold of 25% just in case
+        n = max(1, min(m, int(t * m)))
+
+        ranking_sieve = Sieve(options.setdefault("hit_count", 1))  # default hit count of 1 just in case
+
+        posting_list_heap = []
+        for term in terms:
+            # get posting lists for terms that occur in corpus:
+            posting_iter = self.__inverted_index.get_postings_iterator(term)
+            first_posting = next(posting_iter, None)
 
             if first_posting is not None:
-                smallest_id = first_posting.document_id
-                posting_lists[term] = (first_posting, term_iter)
+                # decided to use min-heap for document-at-a-time traversal
+                heappush(posting_list_heap, (first_posting.document_id, (term, first_posting, posting_iter)))
 
-        postings_left = True
-
-        while postings_left:
-            postings_left = False
-            smallest_id = int(1e64)
-            for (term, (posting, _)) in posting_lists.items():
-                if posting is not None and posting.document_id < smallest_id:
-                    smallest_id = posting.document_id
-
-            if smallest_id < 0:
-                break
+        while len(posting_list_heap) > 0:
+            # peek the smallest document_id and associated posting
+            smallest_id = posting_list_heap[0][0]
+            posting = posting_list_heap[0][1][1]
+            if posting is None:
+                continue
 
             ranker.reset(smallest_id)
-            hit_count = 0
-            for (term, (posting, iterator)) in posting_lists.items():
-                if posting is not None and posting.document_id == smallest_id:
-                    hit_count +=1
-                    ranker.update(term, term_counter[term], posting)
-                    posting_lists[term] = (next(iterator, None), iterator)
-                    postings_left = True
+            term_occurrences = 0
 
-            if hit_count >= n:
+            while posting.document_id == smallest_id:
+                posting_tuple = heappop(posting_list_heap)  # pop next posting for current doc
+                term = posting_tuple[1][0]
+                posting = posting_tuple[1][1]
+                posting_iter = posting_tuple[1][2]
+
+                # rank and count term occurrences in doc
+                ranker.update(term, term_multiplicity[term], posting)
+                term_occurrences += 1
+
+                posting = next(posting_iter, None)
+                if posting is not None:  # if posting list not exhausted, push next posting back on heap
+                    heappush(posting_list_heap, (posting.document_id, (term, posting, posting_iter)))
+
+                if len(posting_list_heap) <= 0:  # stop if no more postings in any list
+                    break
+
+                posting = posting_list_heap[0][1][1]  # peek next smallest id and posting on heap
+
+            if term_occurrences >= n:  # sift all documents with at least n occurrences of terms
                 score = ranker.evaluate()
                 ranking_sieve.sift(score, smallest_id)
 
         winners = ranking_sieve.winners()
         for (score, doc_id) in winners:
             yield {"score": score, "document": self.__corpus.get_document(doc_id)}
-        # raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+
+# example run of assignments.py c-1:
+r"""
+(venv) PS E:\Documents\in3120-2024\tests> python.exe .\assignments.py c-1
+test_canonicalized_corpus (test_simplesearchengine.TestSimpleSearchEngine.test_canonicalized_corpus) ... ok
+test_document_at_a_time_traversal_mesh_corpus (test_simplesearchengine.TestSimpleSearchEngine.test_document_at_a_time_traversal_mesh_corpus) ... ok
+test_mesh_corpus (test_simplesearchengine.TestSimpleSearchEngine.test_mesh_corpus) ... ok
+test_synthetic_corpus (test_simplesearchengine.TestSimpleSearchEngine.test_synthetic_corpus) ... ok
+test_uses_yield (test_simplesearchengine.TestSimpleSearchEngine.test_uses_yield) ... ok
+
+----------------------------------------------------------------------
+Ran 5 tests in 0.528s
+
+OK
+"""
+
+# example run of repl.py c-1:
+r"""
+Indexing English news corpus...
+Enter a query and find matching documents.
+Lookup options are {'debug': False, 'hit_count': 5, 'match_threshold': 0.5}.
+Tokenizer is SimpleTokenizer.
+Ranker is SimpleRanker.
+Ctrl-C to exit.
+query>pollUtion waTer
+[{'document': {'document_id': 9699, 'fields': {'body': 'While there are not many people in the water during the winter months, there are plenty playing by the shore with their jeans rolled up just enough so that they can feel the cool water lap up against their feet.'}},
+  'score': 2.0},
+ {'document': {'document_id': 7398, 'fields': {'body': 'The elevated salt levels in the water threatened some of the wildlife in the area that depend on a supply of fresh water.'}},
+  'score': 2.0},
+ {'document': {'document_id': 5854, 'fields': {'body': 'Polluted air, on the other hand, contains water-soluble particles, leading to clouds with more, yet smaller, water droplets.'}},
+  'score': 2.0},
+ {'document': {'document_id': 4515, 'fields': {'body': 'Kate Kralman, who shot the video of the MAX going through the water, was helping a friend load equipment nearby when she saw one light rail train go through the water.'}},
+  'score': 2.0},
+ {'document': {'document_id': 354, 'fields': {'body': "A lot of people are disputing that climate change is a reality because they don't see everybody going under water."}},
+  'score': 1.0}]
+Evaluation took 0.0002347999998164596 seconds.
+"""
