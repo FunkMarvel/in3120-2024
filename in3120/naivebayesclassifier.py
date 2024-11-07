ce23ba2f70d5e4f04197dde585dadbb1d26f2a7d
Anders Petershagen Åsbø <39097620+plantepotte@users.noreply.github.com>
2024-11-07 18:04:35 +0100

    Update naivebayesclassifier.py

--- in3120/naivebayesclassifier.py
+++ in3120/naivebayesclassifier.py
@@ -4,6 +4,8 @@ import itertools
 import math
 from collections import Counter
 from typing import Any, Dict, Iterable, Iterator
+
+from . import Sieve
 from .dictionary import InMemoryDictionary
 from .normalizer import Normalizer
 from .tokenizer import Tokenizer
@@ -50,7 +52,7 @@ class NaiveBayesClassifier:
         Estimates all prior probabilities (or, rather, log-probabilities) needed for
         the naive Bayes classifier.
         """
-        num_docs = sum((training_set[category].size() for category in training_set))
+        num_docs = sum(training_set[category].size() for category in training_set)
 
         for category in training_set:
             self.__priors[category] = math.log(training_set[category].size() / num_docs)
@@ -66,16 +68,30 @@ class NaiveBayesClassifier:
                 for term in all_terms:
                     self.__vocabulary.add_if_absent(term)
 
-
     def __compute_posteriors(self, training_set, fields) -> None:
         """
         Estimates all conditional probabilities (or, rather, log-probabilities) needed for
         the naive Bayes classifier.
         """
-        for category in training_set:
-            self.__conditionals[category] = {}
-            num_docs = training_set[category].size()
-            tot_term_occurrences = 0
+        for category in training_set: # populate conditionals with 1 smoothing occurrence per term:
+            self.__conditionals[category] = {term: 1 for term, _ in self.__vocabulary}
+            tot_term_occurrences = len(self.__vocabulary)
+
+            for document in training_set[category]:  # iterable handling borrowed from invertedindex.py:
+                all_terms = itertools.chain.from_iterable(self.__get_terms(document.get_field(f, "")) for f in fields)
+
+                term_frequencies = Counter(all_terms)  # count actual occurrences
+                tot_term_occurrences += sum(freq for freq in term_frequencies.values())
+
+                for term, freq in term_frequencies.items():  # add up for each term
+                    self.__conditionals[category][term] += freq
+
+            # calculate log-probabilities for current class:
+            for term, _ in self.__vocabulary:
+                freq = self.__conditionals[category][term]
+                self.__conditionals[category][term] = math.log(freq / tot_term_occurrences)
+
+
 
     def __get_terms(self, buffer) -> Iterator[str]:
         """
@@ -114,4 +130,50 @@ class NaiveBayesClassifier:
         The results yielded back to the client are dictionaries having the keys "score" (float) and
         "category" (str).
         """
-        #TODO: raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        scores = Sieve(len(self.__conditionals))
+        all_terms = self.__get_terms(buffer)
+        term_frequencies = Counter(all_terms)
+
+        for category, conditionals in self.__conditionals.items():
+            score = self.__priors[category]
+            for term, freq in term_frequencies.items():
+                if term not in self.__vocabulary:
+                    continue
+                score += conditionals[term]*freq
+
+            scores.sift(score, category)
+
+        yield from ({"score": score, "category": cat} for score, cat in scores.winners())
+
+# example run of assignments.py e-1:
+r"""
+(venv) PS E:\Documents\in3120-2024\tests> python.exe .\assignments.py e-1
+test_china_example_from_textbook (test_naivebayesclassifier.TestNaiveBayesClassifier.test_china_example_from_textbook) ... ok
+test_language_detection_trained_on_some_news_corpora (test_naivebayesclassifier.TestNaiveBayesClassifier.test_language_detection_trained_on_some_news_corpora) ... ok
+test_predict_movie_genre_from_movie_title (test_naivebayesclassifier.TestNaiveBayesClassifier.test_predict_movie_genre_from_movie_title) ... ok
+test_predict_name_of_search_engine_from_description (test_naivebayesclassifier.TestNaiveBayesClassifier.test_predict_name_of_search_engine_from_description) ... ok
+test_scores_are_sorted_descending (test_naivebayesclassifier.TestNaiveBayesClassifier.test_scores_are_sorted_descending) ... ok
+test_uses_yield (test_naivebayesclassifier.TestNaiveBayesClassifier.test_uses_yield) ... ok
+
+----------------------------------------------------------------------
+Ran 6 tests in 1.692s
+
+OK
+"""
+
+# example run of repl.py e-1:
+r"""
+(venv) PS E:\Documents\in3120-2024\tests> python.exe .\repl.py e-1       
+Initializing naive Bayes classifier from news corpora...
+Enter some text and classify it into ['en', 'no', 'da', 'de'].
+Returned scores are log-probabilities.
+Ctrl-C to exit.
+text>Seks mine jarlar heime vera, gøyme det gullet balde, andre seks på heidningalando då svinga dei jørni kalde!
+[{'category': 'no', 'score': -98.75397295627718},
+ {'category': 'da', 'score': -106.06572984644481},
+ {'category': 'de', 'score': -136.14885654039784},
+ {'category': 'en', 'score': -136.317660020898}]
+Evaluation took 0.0002134000005753478 seconds.
+text>
+Bye!
+"""
